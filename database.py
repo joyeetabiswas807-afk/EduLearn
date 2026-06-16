97db83306b309fbeb654e202ea8c872298ec41f3
Joyeeta Biswas <joyeetabiswas807@gmail.com>
2026-06-16 21:50:26 +0530

    Added enrollments table

--- database.py
+++ database.py
@@ -5,12 +5,23 @@ def create_database():
 
     cursor = conn.cursor()
 
+    # Users table
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS users(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
         email TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL
+    )
+    """)
+
+    # Course enrollments table
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS enrollments(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        course_name TEXT NOT NULL,
+        FOREIGN KEY(user_id) REFERENCES users(id)
     )
     """)
 
